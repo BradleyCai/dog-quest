4b5f36741f36e2b0886d8395e5952cf0fb8db708
Jonathan Ho <ho8584@gmail.com>
2019-04-23 17:43:43 -0700

    Added variation basic enemy attacks

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+
+    int bulletLayer; // stores which layer 'bullet' is in unity
+    float attackCooldown;
+
+    [SerializeField] float attackRate = 0.5f;
+    [SerializeField] float attackAngle = 0;
+    [SerializeField] float speed = 1;
+    [SerializeField] float damage = 1f;
+
+	// Start is called before the first frame update
+    void Start() {
+    	bulletLayer = gameObject.layer; // layer in unity should be set to bullet
+    	attackCooldown = 0f;
+    }
+
+    // Update is called once per frame
+    void Update() {  
+        attackCooldown -= Time.deltaTime;        		
+
+        // can attack again, FIRE!!!!
+    	if (attackCooldown <= 0) {
+    		attackCooldown = attackRate; // just fired, reset cooldown
+
+			Quaternion rot = Quaternion.Euler(0, 0, attackAngle); // sets bullet rotation
+            GameObject bullet = (GameObject)Instantiate(bulletPrefab, gameObject.transform.position, rot); 
+            bullet.layer = bulletLayer;  // sets gameObject to bullet
+            bullet.GetComponent<BulletTrajectoryLinear>().speed = speed; // sets speed for <BulletTrajectoryLinear>
+    	}
+    }
+}
---
+++ Assets/Scripts/RemoveFromPlay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveFromPlay : MonoBehaviour {
+
+    // Update is called once per frame
+    void Update() {
+    	Vector3 pos = transform.position;
+
+        // outside y camera boundaries, remove from play
+        if (pos.y > Camera.main.orthographicSize || pos.y < -Camera.main.orthographicSize) {
+        	Destroy(gameObject);
+        }
+
+        // outside of x camera boundaries, remove from play
+        float screenRatio = (float)Screen.width / (float)Screen.height;
+        float widthOrtho = Camera.main.orthographicSize * screenRatio;
+        if (pos.x > widthOrtho || pos.x < -widthOrtho) {
+            Destroy(gameObject);
+        }
+    }
+
+}
