5f31cdd10db02b2f437fea450b95f7770ba62ad1
ShenqiYe <yeshenqi0@gmail.com>
2019-04-27 11:15:15 +0800

    Issue8:Implement Simple GUI
    main GUI:
    - the 'PAUSE' button which can control the game to resume and restart
    - the bloodbar can follow the enemy so that the player can see how much blood remains clearly
    - the 'Kill' text can show the player's score
    
    Note:
    - most of the functions are fragmentary and only after the basic interface between enemies and player are built can them be filled
    - the GUI can be polished with more pictures later

---
+++ Assets/Scripts/EnemyHP.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHP : MonoBehaviour
+{
+    private Vector3 vel;
+    public float speed = 1;
+
+    private Transform t;
+
+    public int maxHealth;
+    public int health;
+    public Slider bloodbar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHealth = 1000;
+        health = 1000;
+        t = this.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Damage
+        health--;
+        bloodbar.value = (float)health / maxHealth;
+
+        if (health <= 0)
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+
+        // Movement
+
+        vel = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0.0f);
+        
+        t.position += Time.deltaTime * speed * vel;
+    }
+}
---
+++ Assets/Scripts/GameUI.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameUI : MonoBehaviour
+{
+    public Slider healthbar;
+
+    public GameObject menu;
+
+    public Text kill_text;
+    private int kill_num;
+
+    private EnemyHP enemy1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //the following codes are just for testing the show of text
+         kill_num++;
+         kill_text.GetComponent<Text>().text = kill_num.ToString();
+    }
+
+    public void scoreSystem()
+    {
+        //when the interface between enemies and player is finished, this function can be called to handle the scoring system
+    }
+
+    public void onPause()
+    {
+        Time.timeScale = 0; //this doesn't affect Update()
+        menu.SetActive(true);
+    }
+
+    public void onResume()
+    {
+        Time.timeScale = 1.0f;
+        menu.SetActive(false);
+    }
+
+    public void onRestart()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
