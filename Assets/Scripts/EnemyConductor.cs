aaf254a317af43620bf984f7655a99e836c1e20b
BradleyCai <bradcai24@gmail.com>
2019-04-30 09:08:29 -0700

    Add new background and fix various bugs

---
+++ Assets/Scripts/EnemyConductor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyConductor : MonoBehaviour
+{
+    public GameObject[] enemies;
+    public float spawnRate;
+
+    private float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time > 1 / spawnRate) {
+            time = 0;
+            Instantiate(enemies[0], gameObject.transform.position, transform.rotation); 
+        }
+        time += Time.deltaTime;
+    }
+}
