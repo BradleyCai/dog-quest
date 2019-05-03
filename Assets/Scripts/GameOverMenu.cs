80debbdefc4d4f1b410f6896e7243d002d60874c
kmilanbanda <kmilanbanda@gmail.com>
2019-05-03 12:19:19 -0700

    Removes text color change feature to be added in separately

--- Assets/Scripts/GameOverMenu.cs
+++ Assets/Scripts/GameOverMenu.cs
@@ -9,32 +9,11 @@ public class GameOverMenu : MonoBehaviour
     public GameObject player;
 
     public Canvas canvas;
-    public Text gameOverText;
-    public Text continueText;
-
-    public Color c1, c2, c3, c4;
-    public float colorPeriod1;
-    public float colorPeriod2;
-    private bool colorPhase1;
-    private bool colorPhase2;
-
-    private float time1;
-    private float time2;
-    
 
     // Start is called before the first frame update
     void Start()
     {
         canvas.enabled = false;
-
-        gameOverText.color = c1;
-        continueText.color = c3;
-
-        time1 = 0;
-        time2 = 0;
-
-        colorPhase1 = true;
-        colorPhase2 = true;
     }
 
     // Update is called once per frame
@@ -45,50 +24,6 @@ public class GameOverMenu : MonoBehaviour
             Debug.Log("player destroyed");
             canvas.enabled = true;
         }
-
-        // text color change 
-
-        if (colorPhase1)
-        {
-            gameOverText.color = Color.Lerp(c1, c2, time1 / colorPeriod1);
-            if (time1 >= colorPeriod1)
-            {
-                time1 = 0;
-                colorPhase1 = false;
-            }
-        }
-        else
-        {
-            gameOverText.color = Color.Lerp(c2, c1, time1 / colorPeriod1);
-            if (time1 >= colorPeriod1)
-            {
-                time1 = 0;
-                colorPhase1 = true;
-            }
-        }
-
-        if (colorPhase2)
-        {
-            continueText.color = Color.Lerp(c3, c4, time2 / colorPeriod2);
-            if (time2 >= colorPeriod2)
-            {
-                time2 = 0;
-                colorPhase2 = false;
-            }
-        }
-        else
-        {
-            continueText.color = Color.Lerp(c4, c3, time2 / colorPeriod2);
-            if (time2 >= colorPeriod2)
-            {
-                time2 = 0;
-                colorPhase2 = true;
-            }
-        }
-        
-
-        time1 += Time.deltaTime;
-        time2 += Time.deltaTime;
     }
 
     public void Retry()
