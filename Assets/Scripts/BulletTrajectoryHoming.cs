782cd2add2d76973bbff69fb9816bd5d79ace89b
crystalarrow <ho8584@gmail.com>
2019-05-07 09:38:51 -0700

    Cleaned up homing bullet code

--- Assets/Scripts/BulletTrajectoryHoming.cs
+++ Assets/Scripts/BulletTrajectoryHoming.cs
@@ -6,27 +6,36 @@ public class BulletTrajectoryHoming : MonoBehaviour {
 
 	Transform player;
 
-	[SerializeField] float rotationSpeed = 90f;
+	[SerializeField] float rotationSpeed = 90f; // change this value to make "homingness" more or less hard
 
     // Update is called once per frame
     void Update() {
+    	
+    	// Player just spawned, find player ship
         if (player == null) {
+        	/*** change the string for player gameobject if player gets renamed ***/
         	GameObject target = GameObject.Find("BasicPlayer"); 
 
+        	// found the player
         	if (target != null) {
         		player = target.transform;
         	}
         }
 
+        // Player doesn't exist in this frame, move to next grame
         if (player == null) {
         	return;
         }
 
-    	Vector3 direction = player.position - transform.position; // distance vector
-        direction.Normalize();
-        float zAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
+        // calculate distance vector
+    	Vector3 direction = player.position - transform.position;
+        direction.Normalize(); 
 
+        // calculate rotation toward player
+        float zAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90; 
+        // convert rotation angle to quaternions
         Quaternion desiredRotation = Quaternion.Euler(0, 0, zAngle);
+        // set rotation toward player
         transform.rotation = Quaternion.RotateTowards(transform.rotation, desiredRotation, rotationSpeed * Time.deltaTime);    
     }
 }
