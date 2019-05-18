7daba17f8e95904badddee516305111c29c394a7
kmilanbanda <kmilanbanda@gmail.com>
2019-05-17 22:41:47 -0700

    Implements ItemDrop script, which allows enemies to drop prefabs in place

---
+++ Assets/Scripts/ItemDrop.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDrop : MonoBehaviour
+{
+    public GameObject item;
+    public int minItems;
+    public int maxItems;
+    private int numItems;
+    private Transform t;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numItems = Random.Range(minItems, maxItems);
+        t = GetComponent<Transform>();
+    }
+
+    private void OnDestroy()
+    {
+        for (int i = 0; i < numItems; i++)
+        {
+            Instantiate(item, t.position, t.rotation);
+        }
+    }
+}
