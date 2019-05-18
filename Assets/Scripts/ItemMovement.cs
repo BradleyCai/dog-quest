cb7ca62a7d0b4d8b4242f99265ba7ec78863cd92
kmilanbanda <kmilanbanda@gmail.com>
2019-05-17 20:54:13 -0700

    Implements dispersion and downward motion for items

--- Assets/Scripts/ItemMovement.cs
+++ Assets/Scripts/ItemMovement.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class ItemMovement : MonoBehaviour
 {
+    private Rigidbody rb;
+
+    public float speed;
+    public float disperseSpeedMultiplier;
+
+    private float timeSeg; // this variable tells the script how long to "disperse" the item
+    public float segRangeMin, segRangeMax;
+    private float time;
+
+    private bool yOnly;
+
+    private Vector3 initialVelocity;
+    private Vector3 finalVelocity;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        yOnly = false;
+        time = 0;
+        timeSeg = Random.Range(segRangeMin, segRangeMax);
+
+        rb.velocity = initialVelocity = new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), 0).normalized * speed * disperseSpeedMultiplier;
+        finalVelocity = initialVelocity / disperseSpeedMultiplier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (time < timeSeg)
+        {
+            rb.velocity = Vector3.Lerp(initialVelocity, finalVelocity, time / timeSeg);
+        }
+        else if (time >= timeSeg && !yOnly)
+        {
+            ChangeVelocity();
+        }
+
+        time += Time.deltaTime;
+    }
+
+    void ChangeVelocity()
+    {
+        rb.velocity = new Vector3(0, -speed, 0);
+        yOnly = true;
     }
 }
