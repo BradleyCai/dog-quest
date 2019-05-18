09a046f6d377d55c7ea03f18784c63f38002fa9f
kmilanbanda <kmilanbanda@gmail.com>
2019-05-17 18:53:20 -0700

    Adds ability for player to pick up items, colors items based on effect

---
+++ Assets/Scripts/ItemEffect.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemEffect : MonoBehaviour
+{
+    public enum Effect { Ammo, ShotSpeed, Homing, NULL};
+
+    public Effect effect;
+
+    private Renderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponentInChildren<Renderer>();
+
+        if (effect == Effect.NULL)
+        {
+            effect = Effect.Ammo;
+        }
+
+        switch(effect)
+        {
+            case Effect.Ammo:
+                rend.material.color = Color.red;
+                break;
+            case Effect.ShotSpeed:
+                rend.material.color = Color.blue;
+                break;
+            case Effect.Homing:
+                rend.material.color = Color.green;
+                break;
+            case Effect.NULL:
+                rend.material.color = Color.clear;
+                break;
+            default:
+                rend.material.color = Color.white;
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ItemPickUp.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickUp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("other: " + other.name);
+        if (other.tag == "Item")
+        {
+            switch(other.gameObject.GetComponent<ItemEffect>().effect)
+            {
+                case ItemEffect.Effect.Ammo:
+                    // Effect
+                    Destroy(other.gameObject);
+                    Debug.Log("Item Consumed: Ammo");
+                    break;
+                case ItemEffect.Effect.ShotSpeed:
+                    // Effect
+                    Destroy(other.gameObject);
+                    Debug.Log("Item Consumed: ShotSpeed");
+                    break;
+                case ItemEffect.Effect.Homing:
+                    // Effect
+                    Destroy(other.gameObject);
+                    Debug.Log("Item Consumed: Homing");
+                    break;
+                case ItemEffect.Effect.NULL:
+                    Destroy(other.gameObject);
+                    Debug.Log("Item Consumed: NULL");
+                    break;
+                default:
+                    Destroy(other.gameObject);
+                    Debug.Log("Item Consumed: Undefined");
+                    break;
+            }
+        }
+    }
+}
