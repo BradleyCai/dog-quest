44ad663ce2f20c6e5a0e5e68db8274d386301b4c
crystalarrow <ho8584@gmail.com>
2019-05-17 18:05:19 -0700

    Basic laser enemy implemented

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -8,7 +8,7 @@ public class Shooter : MonoBehaviour {
 
     int bulletLayer; // stores which layer 'bullet' is in unity
     float attackCooldown;
-    float laserCooldown;
+
 
     /** basic linear attacks **/
     [Header("Basic Bullet (default)")]
@@ -24,11 +24,9 @@ public class Shooter : MonoBehaviour {
 
     [Header("Laser")]
     [SerializeField] bool laser = false;
-    [SerializeField] LineRenderer lineRenderer;
-    [SerializeField] float laserFireRate = 2f;
-    float laserTimer = 2f;
-    float laserTracking = 0f;
-    float laserOn = 0f;
+    [SerializeField] float laserFireTime = 2f;
+    [SerializeField] float laserOffTime = 2f;
+    float laserCooldown;
 
     Transform gun;
 
@@ -36,15 +34,13 @@ public class Shooter : MonoBehaviour {
     void Start() {
     	bulletLayer = gameObject.layer; // layer in unity should be set to bullet
     	attackCooldown = 0f;
-        laserTracking = laserTimer;
+        laserCooldown = laserFireTime;
     }
 
     // Update is called once per frame
     void Update() {  
-        attackCooldown -= Time.deltaTime;   		
-        laserCooldown -= Time.deltaTime;
-
         if (!laser) {
+            attackCooldown -= Time.deltaTime;
             // can attack again, FIRE!!!!
         	if (attackCooldown <= 0) {
             	BasicAttack();
@@ -57,60 +53,26 @@ public class Shooter : MonoBehaviour {
         }
         // attack with laser
         if (laser) {
-            FireLaser();
+            if (laserCooldown >= 0) { // fires laser for this amount of time
+                FireLaser();
+            }
+            else { // laser stops firing for this amount of time
+                laserCooldown -= Time.deltaTime;
+                if (laserCooldown < -laserOffTime) {
+                    laserCooldown = laserFireTime;
+                }
+            }
+
         }   
     }
 
     void FireLaser() {
-        laserTracking -= Time.deltaTime;
-        laserOn -= Time.deltaTime;
-        GameObject target = GameObject.Find("BasicPlayer");  
-        lineRenderer.SetPosition(0, gameObject.transform.position);
-        float laserOffset = 0.5f;
-
-        if (!lineRenderer.enabled) {
-            lineRenderer.enabled = true;
-            lineRenderer.SetWidth(0.03f, 0.03f);
-        }
-
-        if (laserCooldown <= 0) {
-
-            if (laserTracking <= 0) {
-                laserCooldown = 1 / laserFireRate;
-                
-                Vector3 offsetPos = target.transform.position;
-                if (gameObject.transform.position.x > target.transform.position.x) { 
-                    offsetPos.x = target.transform.position.x + laserOffset;
-                }
-                else if (gameObject.transform.position.x < target.transform.position.x) {
-                    offsetPos.x = target.transform.position.x - laserOffset;
-                } 
-                else {
-                    offsetPos.x = target.transform.position.x;
-                }      
-                if (gameObject.transform.position.y > target.transform.position.y) { 
-                    offsetPos.y = target.transform.position.y + laserOffset;
-                }
-                else if (gameObject.transform.position.y < target.transform.position.y) {
-                    offsetPos.y = target.transform.position.y - laserOffset;
-                }
-                else {
-                    offsetPos.y = target.transform.position.y;
-                }
-
-                lineRenderer.SetWidth(0.25f, 0.25f);
-                lineRenderer.SetPosition(1, offsetPos);
-
-                laserTracking = laserTimer; 
-                laserOn = laserFireRate;
-            }
-        }
-
-        if (laserOn <= 0) {
-            lineRenderer.SetWidth(0.03f, 0.03f);
-            lineRenderer.SetPosition(1, target.transform.position);
-        }
-
+        Quaternion rot = Quaternion.Euler(0, 0, angleOffset); // sets bullet rotation
+        bullet = (GameObject)Instantiate(bulletPrefab, gameObject.transform.position, rot * transform.rotation); 
+        bullet.layer = bulletLayer;  // sets gameObject to bullet
+        bullet.GetComponent<BulletTrajectoryLinear>().speed = speed; // sets speed for <BulletTrajectoryLinear>
+        bullet.GetComponent<BulletTrajectoryLinear>().damage = damage; // sets damage for <BulletTrajectoryLinear>
+        laserCooldown -= Time.deltaTime;
     }
 
     void BasicAttack() {
