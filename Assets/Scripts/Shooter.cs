4cffb4ed7757f604741a54110a987d01a5c717ed
crystalarrow <ho8584@gmail.com>
2019-05-17 19:58:25 -0700

    Added homing laser enemies

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -24,16 +24,20 @@ public class Shooter : MonoBehaviour {
 
     [Header("Laser")]
     [SerializeField] bool laser = false;
+    [SerializeField] bool homingLaser = false;
     [SerializeField] float laserFireTime = 2f;
     [SerializeField] float laserOffTime = 2f;
+    bool fired = false;
     float laserCooldown;
 
+    Quaternion rot;
     Transform gun;
+    Transform player;
 
-	// Start is called before the first frame update
+    // Start is called before the first frame update
     void Start() {
-    	bulletLayer = gameObject.layer; // layer in unity should be set to bullet
-    	attackCooldown = 0f;
+        bulletLayer = gameObject.layer; // layer in unity should be set to bullet
+        attackCooldown = 0f;
         laserCooldown = laserFireTime;
     }
 
@@ -42,8 +46,8 @@ public class Shooter : MonoBehaviour {
         if (!laser) {
             attackCooldown -= Time.deltaTime;
             // can attack again, FIRE!!!!
-        	if (attackCooldown <= 0) {
-            	BasicAttack();
+            if (attackCooldown <= 0) {
+                BasicAttack();
                 
                 // if the bullet is homing, set "homing" effectiveness 
                 if (homing) {
@@ -51,23 +55,71 @@ public class Shooter : MonoBehaviour {
                 }
             }
         }
+
         // attack with laser
         if (laser) {
             if (laserCooldown >= 0) { // fires laser for this amount of time
-                FireLaser();
+                if (homingLaser && laser) { // homing laser
+                    FireHomingLaser();
+                }
+                else { // strait lasers
+                    FireLaser();
+                }
             }
             else { // laser stops firing for this amount of time
                 laserCooldown -= Time.deltaTime;
                 if (laserCooldown < -laserOffTime) {
                     laserCooldown = laserFireTime;
                 }
+                fired = false;
             }
 
         }   
     }
 
+    void FireHomingLaser() {
+        if (homingLaser) {
+            if (!fired) {
+                // Player just spawned, find player ship
+                if (player == null) {
+                    /*** change the string for player gameobject if player gets renamed ***/
+                    GameObject target = GameObject.Find("BasicPlayer"); 
+
+                    // found the player
+                    if (target != null) {
+                        player = target.transform;
+                    }
+                }
+
+                // Player doesn't exist in this frame, move to next frame
+                if (player == null) {
+                    return;
+                }
+
+                // calculate distance vector
+                Vector3 direction = player.position - transform.position;
+                direction.Normalize(); 
+                // calculate rotation toward player
+                float zAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90; 
+                // convert rotation angle to quaternions
+                Quaternion desiredRotation = Quaternion.Euler(0, 0, zAngle);
+                // set rotation toward player
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, desiredRotation, rotationSpeed * Time.deltaTime);
+
+                rot = desiredRotation;
+                fired = true;
+            }
+        }
+
+        bullet = (GameObject)Instantiate(bulletPrefab, gameObject.transform.position, rot * transform.rotation); 
+        bullet.layer = bulletLayer;  // sets gameObject to bullet
+        bullet.GetComponent<BulletTrajectoryLinear>().speed = speed; // sets speed for <BulletTrajectoryLinear>
+        bullet.GetComponent<BulletTrajectoryLinear>().damage = damage; // sets damage for <BulletTrajectoryLinear>
+        laserCooldown -= Time.deltaTime;
+    }
+
     void FireLaser() {
-        Quaternion rot = Quaternion.Euler(0, 0, angleOffset); // sets bullet rotation
+        rot = Quaternion.Euler(0, 0, angleOffset);
         bullet = (GameObject)Instantiate(bulletPrefab, gameObject.transform.position, rot * transform.rotation); 
         bullet.layer = bulletLayer;  // sets gameObject to bullet
         bullet.GetComponent<BulletTrajectoryLinear>().speed = speed; // sets speed for <BulletTrajectoryLinear>
@@ -78,7 +130,7 @@ public class Shooter : MonoBehaviour {
     void BasicAttack() {
         attackCooldown = 1 / attackRate; // just fired, reset cooldown
 
-        Quaternion rot = Quaternion.Euler(0, 0, angleOffset); // sets bullet rotation
+        rot = Quaternion.Euler(0, 0, angleOffset); // sets bullet rotation
         bullet = (GameObject)Instantiate(bulletPrefab, gameObject.transform.position, rot * transform.rotation); 
         bullet.layer = bulletLayer;  // sets gameObject to bullet
         bullet.GetComponent<BulletTrajectoryLinear>().speed = speed; // sets speed for <BulletTrajectoryLinear>
