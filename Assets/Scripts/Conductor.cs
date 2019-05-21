8d67a40f32d7493547ff641f8e076b623056f185
BradleyCai <bradcai24@gmail.com>
2019-05-20 23:16:20 -0700

    Update the path follower to allow for local positioning instead of global, and implement conductor script

---
+++ Assets/Scripts/Conductor.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conductor : MonoBehaviour
+{
+    public bool loop;
+    [System.Serializable] public struct Actor {
+        public GameObject gameObject;
+        public Vector3 position;
+        public float time;
+    }
+    public Actor[] actors;
+    private int currActor;
+    private float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currActor = 0;
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if (currActor >= actors.Length) {
+            if (loop) {
+                currActor = 0;
+                time = 0;
+            }
+            else {
+                this.gameObject.SetActive(true);
+            }
+        }
+        else if (actors[currActor].time == -1) {
+            if (currActor > 0 && !actors[currActor - 1].gameObject.activeInHierarchy) {
+                Instantiate(actors[currActor].gameObject, actors[currActor].position, Quaternion.identity);
+                currActor += 1;
+            }
+        }
+        else if (time > actors[currActor].time) {
+            Instantiate(actors[currActor].gameObject, actors[currActor].position, Quaternion.identity);
+            currActor += 1;
+        }
+    }
+}
