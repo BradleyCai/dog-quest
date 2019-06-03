14dd33a8e6a39f581b820bc4168b0ee767d0bfa7
kmilanbanda <kmilanbanda@gmail.com>
2019-06-03 14:43:45 -0700

    Creates SpringBossDemo with Spring Boss and two associated scripts

---
+++ Assets/Scripts/SpringBossFlowerShooter.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringBossFlowerShooter : MonoBehaviour
+{
+    private Transform player;
+
+    private float waitTimeTeleport;
+    private float waitTimeFire;
+    //Note: Shooter's "attack rate" is 1 / (waitTimeTeleport + waitTimeFire)
+    private bool hasTeleported;
+
+    public GameObject[] gunSet;
+    private bool gunSetActivated;
+
+    private float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        if (player == null)
+        {
+            this.gameObject.SetActive(false);
+            return;
+        }
+
+        DeactivateGunSet();
+        gunSetActivated = false;
+
+        waitTimeTeleport = this.GetComponentInParent<SpringBoss>().waitTimeTeleport;
+        waitTimeFire = this.GetComponentInParent<SpringBoss>().waitTimeFire;
+        SetAttackRate();
+
+        hasTeleported = false;
+
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time >= waitTimeTeleport && !hasTeleported)
+        {
+            this.gameObject.transform.position = player.position;
+            hasTeleported = true;
+        }
+
+        if (time >= waitTimeTeleport + waitTimeFire)
+        {
+            time = 0;
+            hasTeleported = false;
+
+            if (!gunSetActivated)
+            {
+                ActivateGunSet();
+            }
+        }
+
+        time += Time.deltaTime;
+    }
+
+    void DeactivateGunSet()
+    {
+        for (int i = 0; i < gunSet.Length; i++)
+        {
+            gunSet[i].GetComponent<Shooter>().enabled = false;
+        }
+        gunSetActivated = false;
+    }
+
+    void ActivateGunSet()
+    {
+        for (int i = 0; i < gunSet.Length; i++)
+        {
+            gunSet[i].GetComponent<Shooter>().enabled = true;
+        }
+        gunSetActivated = true;
+    }
+
+    void SetAttackRate()
+    {
+        for (int i = 0; i < gunSet.Length; i++)
+        {
+            gunSet[i].GetComponent<Shooter>().attackRate = 1 / (waitTimeTeleport + waitTimeFire);
+        }
+    }
+}
