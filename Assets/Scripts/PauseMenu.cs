50505f2144e5472f942cd5d41603d1f6f5fdbfcb
ShenqiYe <yeshenqi0@gmail.com>
2019-06-04 09:10:49 -0700

    fix mute button in pause menu

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -8,13 +8,33 @@ public class PauseMenu : MonoBehaviour
 {
     public Canvas canvas;
     public bool onOff; // I just wanted a value that is separate from canvas.enabled in case that causes problems
-    
+     private bool isMute;
+    public Button soundBt;
+    public Sprite mute_img;
+    public Sprite sound_img;
+    public AudioSource background_music; //change this public interface into a suitable music
+    Button btn;
     // Start is called before the first frame update
     void Start()
     {
         canvas.enabled = false;
         onOff = false;
         Time.timeScale = 1.0f;
+isMute = false;
+        btn = soundBt.GetComponent<Button>();
+        btn.onClick.AddListener(delegate ()
+        {
+            isMute = !isMute;
+            MuteControl(isMute);
+            if (isMute)
+            {
+                btn.GetComponent<Image>().sprite = mute_img;
+            }
+            else
+            {
+                btn.GetComponent<Image>().sprite = sound_img;
+            }
+        }); //change the image when click on the sound button
     }
 
     // Update is called once per frame
@@ -57,5 +77,16 @@ public class PauseMenu : MonoBehaviour
     public void Return() // returns to title screen
     {
         SceneManager.LoadScene("TitleScreen"); // this means scene 0 needs to be the title screen
+    }
+private void MuteControl(bool flag)//control on and off of BGM
+    {
+        if (!isMute)
+        {
+            background_music.Play();
+        }
+        else
+        {
+            background_music.Pause();
+        }
     }
 }
