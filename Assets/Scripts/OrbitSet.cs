8c57382c8f9ce7e219cea5140f7cb0d4853d03c2
kmilanbanda <kmilanbanda@gmail.com>
2019-06-04 07:30:25 -0700

    Finishes space boss

---
+++ Assets/Scripts/OrbitSet.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitSet : MonoBehaviour
+{
+    public GameObject[] objects; // objects in the orbit set
+
+    private bool destroyThis;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.name = "OrbitSet";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        destroyThis = true;
+        foreach (GameObject obj in objects)
+        {
+            if (obj != null)
+            {
+                destroyThis = false;
+            }
+        }
+
+        if (destroyThis)
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/SpaceBoss.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceBoss : MonoBehaviour
+{
+    public float spawnDelay; // variable for setting the time between spawns
+    private float time;
+
+    public GameObject orbitSet;
+
+    public GameObject shield;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.Find("OrbitSet") == null)
+        {
+            shield.SetActive(false);
+            if (time >= spawnDelay)
+            {
+                shield.SetActive(true);
+                time = 0;
+                Instantiate(orbitSet, transform);
+            }
+            time += Time.deltaTime;
+        }
+
+        if (GameObject.Find("OrbitSet") != null)
+        {
+            shield.SetActive(true);
+        }
+    }
+}
